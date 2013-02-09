b3e76ab5ef128c91bf7fdd2c66f97a265038f863
Solman Vaisman Gonzalez <solman@mimanzana.com>
2013-02-08 23:30:52 -0500

    Agenda

--- application/modules/admin/controllers/AgendaController.php
+++ application/modules/admin/controllers/AgendaController.php
@@ -11,6 +11,26 @@ class Admin_AgendaController extends App_Controller_Action
     
     public function indexAction()
     {
+        //CSS
+        $this->view->headLink()->appendStylesheet(
+            $this->getConfig()->app->mediaUrl . '/css/fullcalendar/fullcalendar.css'
+        );
+        $this->view->headLink()->appendStylesheet(
+            $this->getConfig()->app->mediaUrl . '/css/fullcalendar/fullcalendar.print.css'
+        );
+        
+        //Javascripts
+        $this->view->headScript()->appendFile(
+            $this->getConfig()->app->mediaUrl . '/js/jquery-1.8.1.min.js'
+        );
+        $this->view->headScript()->appendFile(
+            $this->getConfig()->app->mediaUrl . '/js/fullcalendar/jquery-ui-1.8.23.custom.min.js'
+        );
+        $this->view->headScript()->appendFile(
+            $this->getConfig()->app->mediaUrl . '/js/fullcalendar/fullcalendar.min.js'
+        );
+
+
         //$this->_flashMessenger->addMessage("Ejemplo");
     }
     
