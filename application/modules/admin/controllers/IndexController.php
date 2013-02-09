c81c686431a483e1cfd0d0e0ffa4d6ce3e76a208
prueba <prueba@gmail.com>
2013-02-08 22:51:47 -0500

    estructura

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -6,6 +6,7 @@ class Admin_IndexController extends App_Controller_Action
     public function indexAction()
     {   
         //$this->_helper->layout->setLayout('layout-prueba');        
+        $this->_flashMessenger->addMessage("aaaaaaa");
     }
     public function index2Action()
     {       
