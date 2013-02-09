67a8e7bee7de6f0cd0d87e6ddfe4c6b9df2a6827
prueba <prueba@gmail.com>
2013-02-08 23:32:19 -0500

    estructura

--- application/modules/admin/controllers/ClienteController.php
+++ application/modules/admin/controllers/ClienteController.php
@@ -12,6 +12,17 @@ class Admin_ClienteController extends App_Controller_Action
     public function indexAction()
     {
         //$this->_flashMessenger->addMessage("Ejemplo");
+        $form = new App_Form_BuscarCliente();
+        $modelCliente = new App_Model_Cliente();
+        
+        $result = $modelCliente->lista();
+        
+        
+        $this->view->form = $form;
+        
+        
+        
+        $this->view->result = $result; 
     }
     
     public function nuevoAction()
