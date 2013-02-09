f97efa9322dcb7659d3d9254c76a011f4e8ce852
prueba <prueba@gmail.com>
2013-02-08 23:47:15 -0500

    estructura

---
+++ scripts/S_1_ALTER_CLIENTE.sql
@@ -0,0 +1,1 @@
+alter table `spa`.`cliente` add column `fechaUltimaVisita` datetime NULL after `fechaNacimiento`, add column `direccion` varchar(100) NULL after `estado`, add column `totalVisitas` int(5) NULL after `direccion`;
