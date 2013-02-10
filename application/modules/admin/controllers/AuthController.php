02f7f9e30a7a2cf60fae3fee3330bd80d8c2ee89
jsteve27 <jsteve.villano@gmail.com>
2013-02-10 14:28:32 -0500

    login 15%

--- application/modules/admin/controllers/AuthController.php
+++ application/modules/admin/controllers/AuthController.php
@@ -8,6 +8,54 @@ class Admin_AuthController extends App_Controller_Action
         $this->view->headScript()->appendFile($this->view->s('/js/auth.js'));            
     }
 
+    public function indexAction(){
+        
+        
+     Zend_Layout::getMvcInstance()->setLayout('login');
+     $form = new App_Form_Login();
+     $this->view->formLogin = $form; 
+     
+      //CSS
+        $this->view->headLink()->appendStylesheet(
+            $this->getConfig()->app->mediaUrl . '/css/screen.css'
+        );
+        $this->view->headLink()->appendStylesheet(
+            $this->getConfig()->app->mediaUrl . '/css/formularios/screen.css'
+        );
+        
+        //JS
+        $this->view->headScript()->appendFile(
+            $this->getConfig()->app->mediaUrl . '/js/formularios/lib/jquery.js'
+        );
+        
+        $this->view->headScript()->appendFile(
+            $this->getConfig()->app->mediaUrl . '/js/formularios/jquery.validate.js'
+        );
+        
+     
+     /*   
+      $this->view->idBody = 'login-bg';
+      $formulario = new Application_Form_Login();
+      $formulario->removeDecorators();
+      $formulario->customDecoratorFile("/form-custom/_formLogin.phtml");
+                  
+      if ($this->getRequest()->isPost()) {
+           if ($formulario->isValid($this->_getAllParams()) && 
+                    $this->autentificateUser($this->_getParam('Login'), 
+                            $this->_getParam('Password'))) {
+                
+                $this->_redirect($this->view->url(array("module" => "admin",
+                            "controller" => "panel",
+                            "action" => "index")));
+            } else {
+                echo "error"; exit();
+            }
+            
+        }
+        $this->view->formLoginAdmin = $formulario;
+       */ 
+    }
+    
     public function loginAction()
     {
         $this->_helper->layout->setlayout('login');
--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -8,6 +8,7 @@ class Admin_IndexController extends App_Controller_Action
     	$this->_flashMessenger->addMessage("Ejemplo");
         //$this->_helper->layout->setLayout('layout-prueba');        
         $this->_flashMessenger->addMessage("aaaaaaa");
+        echo "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";    
     }
     public function index2Action()
     {       
