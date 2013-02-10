6898ed93e306000ede440b12b1b972869a1f92b3
prueba <prueba@gmail.com>
2013-02-10 15:43:52 -0500

    message

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -4,11 +4,8 @@ class Admin_IndexController extends App_Controller_Action
 {
 
     public function indexAction()
-    {   
-    	$this->_flashMessenger->addMessage("Ejemplo");
-        //$this->_helper->layout->setLayout('layout-prueba');        
-        $this->_flashMessenger->addMessage("aaaaaaa");
-        echo "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";    
+    {       	
+        
     }
     public function index2Action()
     {       
