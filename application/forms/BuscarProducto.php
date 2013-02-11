cdbb88dab900838da274ac22d04e73b56cb28b39
prueba <prueba@gmail.com>
2013-02-10 19:25:15 -0500

    a

---
+++ application/forms/BuscarProducto.php
@@ -0,0 +1,21 @@
+<?php
+
+class App_Form_BuscarProducto extends App_Form
+{
+    public function init() {        
+        parent::init();
+        
+        $e = new Zend_Form_Element_Text('idProducto');
+        $e->setAttrib('class', 'span8');
+        $this->addElement($e);
+        
+        $e = new Zend_Form_Element_Text('nombreProducto');
+        $e->setAttrib('class', 'span8');
+        $this->addElement($e);
+        
+        $e = new Zend_Form_Element_Submit('buscar');
+        $e->setLabel('Buscar')->setAttrib('class', 'btn pull-right');
+        $this->addElement($e);        
+        
+    }
+}
